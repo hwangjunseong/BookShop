b128c1e22e3b6688e619164b90b002837eea30cd
hwangjunseong <junseong5832@gmail.com>
2024-10-25 14:32:27 +0900

    fix auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,22 +7,20 @@ const { accessVerify } = require("../util/jwt-util");
 module.exports = (req, res, next) => {
   const authHeader = req.get("Authorization");
   if (!authHeader) {
-    req.isAuth = false;
-    return next(); // 아래코드를 실행하지않고 next미들웨어로 넘어감
+    const error = new Error(" Not authenticated");
+    error.statusCode = 401;
+    throw error;
   }
   const token = authHeader.split(" ")[1];
   let decodedToken = accessVerify(token); //bear whatever
 
   //검증에 실패하거나 토큰이 만료되었음
   if (!decodedToken.isAuth) {
-    req.isAuth = false;
-
     // 토큰 만료 에러인 경우 , 잘못된 토큰인 경우 , 그 외 다른 오류 처리
     const error = new Error(`${decodedToken.err}: ${decodedToken.message}.`);
     error.statusCode = decodedToken.statusCode;
     return next(error);
   }
   req.userId = decodedToken.userId;
-  req.isAuth = true;
   next();
 };
